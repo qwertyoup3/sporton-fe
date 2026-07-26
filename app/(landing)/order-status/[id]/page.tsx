52b8898c312a29922a373fa5ecc08c5dc5b15adc
qwertyoup3 <141247977+qwertyoup3@users.noreply.github.com>
2026-07-26 21:39:11 +0700

    update padding card

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -11,13 +11,14 @@ const OrderStatus = async ({ params }: TPageProps) => {
   console.log("transaction", transaction);
 
   return (
-    <main className="bg-gray-100 min-h-[80vh]">
-      <div className="max-w-5xl mx-auto pt-40 pb-20">
+    <main className="bg-gray-100 min-h-[80vh] pb-24">
+      <div className="max-w-5xl mx-auto pt-40 pb-10">
         <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
+        
+        {transaction.status === "pending" && <OrderSubmitted />}
+        {transaction.status === "paid" && <OrderConfirmed />}
+        {transaction.status === "rejected" && <OrderRejected />}
       </div>
-      {transaction.status === "pending" && <OrderSubmitted />}
-      {transaction.status === "paid" && <OrderConfirmed />}
-      {transaction.status === "rejected" && <OrderRejected />}
     </main>
   );
 };
